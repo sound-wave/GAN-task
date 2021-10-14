4c1142f752d489ebfb602d99acaa7866da584026
sentinelprime619 <72153964+sentinelprime619@users.noreply.github.com>
2021-10-15 00:53:19 +0530

    Update ops.py

--- ops.py
+++ ops.py
@@ -8,7 +8,6 @@ import tensorflow as tf
 from utils import *
 
 class batch_norm(object):
-    """Code modification of http://stackoverflow.com/a/33950177"""
     def __init__(self, epsilon=1e-5, momentum = 0.9, name="batch_norm"):
         with tf.variable_scope(name):
             self.epsilon = epsilon
@@ -17,8 +16,10 @@ class batch_norm(object):
             self.name = name
 
     def __call__(self, x, train=True):
-        return tf.contrib.layers.batch_norm(x, decay=self.momentum, updates_collections=None, epsilon=self.epsilon,
-                                            center=True, scale=True, is_training=train, scope=self.name)
+        return tf.contrib.layers.batch_norm(x, decay=self.momentum, 
+                                            updates_collections=None, 
+                                            epsilon=self.epsilon, center=True, scale=True, 
+                                            is_training=train, scope=self.name)
 
 def conv2d(input_, output_dim,
            k_h=5, k_w=5, d_h=2, d_w=2, stddev=0.02,
@@ -26,10 +27,11 @@ def conv2d(input_, output_dim,
     with tf.variable_scope(name):
         w = tf.get_variable('w', [k_h, k_w, input_.get_shape()[-1], output_dim],
                             initializer=tf.truncated_normal_initializer(stddev=stddev))
+        
         conv = tf.nn.conv2d(input_, w, strides=[1, d_h, d_w, 1], padding='SAME')
 
         biases = tf.get_variable('biases', [output_dim], initializer=tf.constant_initializer(0.0))
-        # conv = tf.reshape(tf.nn.bias_add(conv, biases), conv.get_shape())
+        
         conv = tf.nn.bias_add(conv, biases)
 
         return conv
@@ -42,8 +44,8 @@ def lrelu(x, leak=0.2, name="lrelu"):
     
 def deconv2d(input_, output_shape, k_h=5, k_w=5, d_h=2, d_w=2, stddev=0.02,
        name="deconv2d", with_w=False):
+    
     with tf.variable_scope(name):
-        # filter : [height, width, output_channels, in_channels]
         w = tf.get_variable('w', [k_h, k_w, output_shape[-1], input_.get_shape()[-1]],
                   initializer=tf.random_normal_initializer(stddev=stddev))
 
